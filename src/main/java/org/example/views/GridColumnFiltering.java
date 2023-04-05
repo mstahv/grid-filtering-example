400cfab3f3357156181286156ff3c067e0131691
Matti Tahvonen <matti@vaadin.com>
2023-04-05 15:59:36 +0300

    #FFS

--- src/main/java/org/example/views/GridColumnFiltering.java
+++ src/main/java/org/example/views/GridColumnFiltering.java
@@ -2,102 +2,66 @@ package org.example.views;
 
 import com.vaadin.demo.domain.DataService;
 import com.vaadin.demo.domain.Person;
-import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.grid.HeaderRow;
-import com.vaadin.flow.component.grid.dataview.GridListDataView;
 import com.vaadin.flow.component.html.Div;
-import com.vaadin.flow.component.html.Label;
-import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
-import com.vaadin.flow.component.textfield.TextFieldVariant;
-import com.vaadin.flow.data.renderer.LitRenderer;
-import com.vaadin.flow.data.renderer.Renderer;
 import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.Route;
-
-import java.util.List;
-import java.util.function.Consumer;
 
 @Route
 public class GridColumnFiltering extends Div {
 
+    Grid<Person> grid = new Grid<>(Person.class, false);
+    private final TextField fullNameFilter;
+    private final TextField emailFilter;
+    private final TextField professionFilter;
+
     public GridColumnFiltering() {
         // tag::snippet1[]
-        Grid<Person> grid = new Grid<>(Person.class, false);
         Grid.Column<Person> nameColumn = grid.addColumn(p -> p.getFirstName() + " " + p.getLastName())
                 .setHeader("Name");
         grid.addColumns("email", "profession");
 
-        List<Person> people = DataService.getPeople();
-        GridListDataView<Person> dataView = grid.setItems(people);
-        PersonFilter personFilter = new PersonFilter(dataView);
+        grid.setItems(DataService.getPeople());
 
-        HeaderRow headerRow = grid.appendHeaderRow();
+        HeaderRow hr = grid.appendHeaderRow();
 
-        headerRow.getCell(nameColumn).setComponent(
-                createFilterHeader("Name", personFilter::setFullName));
-        headerRow.getCell(grid.getColumnByKey("email")).setComponent(
-                createFilterHeader("Email", personFilter::setEmail));
-        headerRow.getCell(grid.getColumnByKey("profession")).setComponent(
-                createFilterHeader("Profession", personFilter::setProfession));
+        fullNameFilter = createSearchField("name",hr.getCell(nameColumn));
+        emailFilter = createSearchField("email", hr.getCell(grid.getColumnByKey("email")));
+        professionFilter = createSearchField("profession", hr.getCell(grid.getColumnByKey("profession")));
         // end::snippet1[]
 
         add(grid);
     }
-
-    private static Component createFilterHeader(String labelText,
-            Consumer<String> filterChangeConsumer) {
+    
+    private TextField createSearchField(String labelText, HeaderRow.HeaderCell cell) {
         TextField textField = new TextField();
         textField.setPlaceholder("Filter by " + labelText + "...");
         textField.setValueChangeMode(ValueChangeMode.LAZY);
         textField.setClearButtonVisible(true);
         textField.setWidthFull();
-        textField.addValueChangeListener(
-                e -> filterChangeConsumer.accept(e.getValue()));
+        textField.addValueChangeListener(e -> filterItems());
+        cell.setComponent(textField);
         return textField;
     }
 
-    private static class PersonFilter {
-        private final GridListDataView<Person> dataView;
-
-        private String fullName;
-        private String email;
-        private String profession;
-
-        public PersonFilter(GridListDataView<Person> dataView) {
-            this.dataView = dataView;
-            this.dataView.addFilter(this::test);
-        }
-
-        public void setFullName(String fullName) {
-            this.fullName = fullName;
-            this.dataView.refreshAll();
-        }
-
-        public void setEmail(String email) {
-            this.email = email;
-            this.dataView.refreshAll();
-        }
-
-        public void setProfession(String profession) {
-            this.profession = profession;
-            this.dataView.refreshAll();
-        }
-
-        public boolean test(Person person) {
-            boolean matchesFullName = matches(person.getFullName(), fullName);
-            boolean matchesEmail = matches(person.getEmail(), email);
-            boolean matchesProfession = matches(person.getProfession(),
-                    profession);
-
-            return matchesFullName && matchesEmail && matchesProfession;
-        }
-
-        private boolean matches(String value, String searchTerm) {
-            return searchTerm == null || searchTerm.isEmpty()
-                    || value.toLowerCase().contains(searchTerm.toLowerCase());
-        }
+    private void filterItems() {
+        grid.setItems(DataService.getPeople().stream()
+                .filter(this::matchesFilters)
+                .toList());
     }
-    
+
+    public boolean matchesFilters(Person person) {
+        boolean matchesFullName = matches(person.getFullName(), fullNameFilter.getValue());
+        boolean matchesEmail = matches(person.getEmail(), emailFilter.getValue());
+        boolean matchesProfession = matches(person.getProfession(),
+                professionFilter.getValue());
+        return matchesFullName && matchesEmail && matchesProfession;
+    }
+    private boolean matches(String value, String searchTerm) {
+        return searchTerm == null || searchTerm.isEmpty()
+                || value.toLowerCase().contains(searchTerm.toLowerCase());
+    }
+
 }
